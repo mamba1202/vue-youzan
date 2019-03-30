21b2301b286c87f9fa3f0c8c248e70e9b10e9b5e
mamba <1612786059@qq.com>
2019-03-30 23:52:18 +0800

    编辑状态：增-减-删(单个和多个)

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -16,6 +16,9 @@ new Vue({
         total: 0,
         editingShop: null, //编辑状态下的商铺
         editingShopIndex: -1,
+        removePopup: false, //是否要删除,
+        removeData: null,
+        removeMsg: ''
     },
     computed: {
         allSelected: {   // allSelected正常情况下全选
@@ -91,8 +94,8 @@ new Vue({
                 list.forEach(shop => {
                     shop.checked = true    //是否结算选中
                     shop.removeChecked = false //是否删除选中（编辑状态下）
-                    shop.editing = false   //是否可编辑
-                    shop.editingMsg = '编辑'  //编辑状态的信息
+                    shop.editing = false   //正常状态    是否可编辑
+                    shop.editingMsg = '编辑'  // 正常状态  编辑字体
                     shop.goodsList.forEach(good => {
                         good.checked = true       //(正常状态下商品是否选中删除）
                         good.removeChecked = false //（编辑状态下商品是否选中删除）
@@ -131,6 +134,84 @@ new Vue({
             })
             this.editingShop = shop.editing ? shop : null
             this.editingShopIndex = shop.editing ? shopIndex : -1
+        },
+        reduce(good) {
+            if (good.number === 1) return
+            axios.post(url.cartReduce, {
+                id: good.id,      //先更改数据库数据再进行本地操作
+                number: 1
+            }).then(res => {
+                good.number--
+            })
+        },
+        add(good) {
+            axios.post(url.addCart, {
+                id: good.id,      //先更改数据库数据再进行本地操作
+                number: 1
+            }).then(res => {
+                good.number++
+            })
+        },
+        remove(shop, shopIndex, good, goodIndex) {
+            this.removePopup = true
+            this.removeData = { shop, shopIndex, good, goodIndex }
+            this.removeMsg = '确定要删除该商品吗？'
+        },
+        removeList() {
+            this.removePopup = true
+            this.removeMsg = `确定将所选${this.removeLists.length}个商品删除？`
+        },
+        removeConfirm() {
+            if (this.$data.removeMsg === '确定要删除该商品吗？') {
+                let { shop, shopIndex, good, goodIndex } = this.removeData
+                axios.post(url.cartRemove, {  //先改变数据库再进行本地操作
+                    id: good.id
+                }).then(res => {
+                    shop.goodsList.splice(goodIndex, 1)  //删除店铺下的商品列表（通过下标删除）
+                    if (!shop.goodsList.length) {         //没有商品时
+                        this.lists.splice(shopIndex, 1)   //删除店铺
+                        //删除后页面还在编辑状态
+                        this.removeShop()                //恢复正常状态
+                    }
+                    this.removePopup = false  //阴影弹框取消
+                      // this.$refs[`goods-${shopIndex}-${goodIndex}`][0].style.left = '0px'
+                })
+            } else {
+                let ids = []
+                this.removeLists.forEach(good => {
+                    ids.push(good.id)
+                })
+                axios.post(url.cartRemoveMore, {
+                    ids
+                }).then(res => {
+                    let arr = []
+                    this.editingShop.goodsList.forEach(good => {
+                        let index = this.removeLists.findIndex(item => {
+                            return item.id == good.id
+                        })
+                        if (index === -1) {
+                            arr.push(good)
+                        }
+                    })
+                    if (arr.length) {
+                        this.editingShop.goodsList = arr
+                    } else {
+                        this.lists.splice(this.editingShopIndex, 1)
+                        this.removeShop()
+                    }
+                    this.removePopup = false
+                })
+            }
+
+        },
+        removeShop() {
+            this.editingShop = null   //恢复正常状态
+            this.editingShopIndex = -1
+            this.lists.forEach(shop => {
+                shop.editing = false        //店铺恢复正常状态
+                shop.editingMsg = '编辑'   //编辑字体
+            })
+
         }
     },
     mixins: [mixin]

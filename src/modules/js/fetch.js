6ba3410d504f14d5e161235210c61691397060fc
mamba <1612786059@qq.com>
2019-04-01 04:42:14 +0800

    修改fetch请求status

--- src/modules/js/fetch.js
+++ src/modules/js/fetch.js
@@ -4,17 +4,18 @@ function fetch(url,data){
     return new Promise((resolve,reject)=>{
         axios.post(url,data).then(res=>{
            let status = res.data.status
-            if(status===200){
+            /* if(status===200){
                 resolve(res)
             }
             if(status===300){
                 location.href="login.html"
                 resolve(res)
             }
-            reject(res)
+            reject(res) */
+            resolve(res)
         })
     }).catch(error=>{
-        eject(res)
+        reject(res)
     })
 }
 

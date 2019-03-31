68288b011b053a7d76dbb738ef8082cb6c81b3ee
mamba <1612786059@qq.com>
2019-04-01 00:41:23 +0800

    vue-router配置

--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -6,6 +6,20 @@ Vue.use(Router)
 let routes = [{
     path: '/',
     component: require('./components/member.vue')
+},{
+   path: '/address',
+   component: require('./components/address.vue'),
+   children:[{
+      path: '',
+      //component: require('./components/all.vue')
+      redirect: 'all'
+   },{
+      path: 'all',
+      component: require('./components/all.vue')
+   },{
+      path: 'form',
+      component: require('./components/form.vue')
+   }]
 }]
 
 //创建router实例
